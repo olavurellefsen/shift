8dc7dddc29946cb957ca623fd644aa2ef74b3e39
Olavur Ellefsen <olavur@ellefsen.fo>
2019-09-02 10:12:56 +0100

    Revert "Merge branch 'develop' of https://github.com/tokni/shift into develop"
    This reverts commit 69800a5ad3c111606d9132ad14e09b7e3c8b1d25.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ChartsTab3 from "./charts/ChartsTab3";
 import ChartsTab4 from "./charts/ChartsTab4";
 import ChartsTab5 from "./charts/ChartsTab5";
 import ChartsTab6 from "./charts/ChartsTab6";
+import About from "./pages/About";
 import PageRenderer from "./pages/PageRenderer";
 import Prerequisites from "./pages/Prerequisites";
 import scenarioCombinations from "./data/scenarioCombinations";
@@ -243,16 +244,7 @@ export class App extends React.Component {
                   />
                 )}
               />
-              <Route path="/about" 
-              render={() => {
-                  return (
-                    <PageRenderer
-                      markdownFiles={[
-                        "descriptions/about.md",
-                      ]}
-                    />
-                  );
-                }} />
+              <Route path="/about" component={About} />
               <Route
                 path="/beskrivelser"
                 render={() => {
