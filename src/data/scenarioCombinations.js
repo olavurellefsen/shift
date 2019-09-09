3568203c860cb501cc52c78691e7a80503c51bb1
olavurellefsen <oe@tokni.com>
2019-09-09 15:28:21 +0100

    Add nameNoOptions and change bio -> opt1

--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -1,16 +1,16 @@
 ﻿export default {scenarioCombinations : {scenarioOptions : [
-{ "id": 0, "name": "Reference_ccs_bio", "short_description": "Reference Bio CCS", "ultra_short_description": "" },
-{ "id": 1, "name": "Reference_bio", "short_description": "Reference Bio", "ultra_short_description": "" },
-{ "id": 2, "name": "Reference_ccs", "short_description": "Reference CCS", "ultra_short_description": "" },
-{ "id": 3, "name": "Reference", "short_description": "Reference", "ultra_short_description": "" },
-{ "id": 4, "name": "Share Optimism_ccs_bio", "short_description": "Share Optimism Bio CCS", "ultra_short_description": "" },
-{ "id": 5, "name": "Share Optimism_bio", "short_description": "Share Optimism Bio", "ultra_short_description": "" },
-{ "id": 6, "name": "Share Optimism_ccs", "short_description": "Share Optimism CCS", "ultra_short_description": "" },
-{ "id": 7, "name": "Share Optimism", "short_description": "Share Optimism", "ultra_short_description": "" },
-{ "id": 8, "name": "Technology Optimism_ccs_bio", "short_description": "Technology Optimism Bio CCS", "ultra_short_description": "" },
-{ "id": 9, "name": "Technology Optimism_bio", "short_description": "Technology Optimism Bio", "ultra_short_description": "" },
-{ "id": 10, "name": "Technology Optimism_ccs", "short_description": "Technology Optimism CCS", "ultra_short_description": "" },
-{ "id": 11, "name": "Technology Optimism", "short_description": "Technology Optimism", "ultra_short_description": "" }
+{ "id": 0, "name": "Reference_ccs_bio", "nameNoOptions": "Reference", "short_description": "Reference Bio CCS", "ultra_short_description": "" },
+{ "id": 1, "name": "Reference_bio", "nameNoOptions": "Reference", "short_description": "Reference Bio", "ultra_short_description": "" },
+{ "id": 2, "name": "Reference_ccs", "nameNoOptions": "Reference", "short_description": "Reference CCS", "ultra_short_description": "" },
+{ "id": 3, "name": "Reference", "nameNoOptions": "Reference", "short_description": "Reference", "ultra_short_description": "" },
+{ "id": 4, "name": "Share Optimism_ccs_bio", "nameNoOptions": "Share Optimism", "short_description": "Share Optimism Bio CCS", "ultra_short_description": "" },
+{ "id": 5, "name": "Share Optimism_bio", "nameNoOptions": "Share Optimism", "short_description": "Share Optimism Bio", "ultra_short_description": "" },
+{ "id": 6, "name": "Share Optimism_ccs", "nameNoOptions": "Share Optimism", "short_description": "Share Optimism CCS", "ultra_short_description": "" },
+{ "id": 7, "name": "Share Optimism", "nameNoOptions": "Share Optimism", "short_description": "Share Optimism", "ultra_short_description": "" },
+{ "id": 8, "name": "Technology Optimism_ccs_bio", "nameNoOptions": "Technology Optimism", "short_description": "Technology Optimism Bio CCS", "ultra_short_description": "" },
+{ "id": 9, "name": "Technology Optimism_bio", "nameNoOptions": "Technology Optimism", "short_description": "Technology Optimism Bio", "ultra_short_description": "" },
+{ "id": 10, "name": "Technology Optimism_ccs", "nameNoOptions": "Technology Optimism", "short_description": "Technology Optimism CCS", "ultra_short_description": "" },
+{ "id": 11, "name": "Technology Optimism", "nameNoOptions": "Technology Optimism", "short_description": "Technology Optimism", "ultra_short_description": "" }
 ],
 regionOptions : [
 { "id": 0, "name": "Denmark", "country": "Denmark", "short_description": "Denmark", "ultra_short_description": "Denmark" },
@@ -18,9 +18,9 @@ regionOptions : [
 { "id": 2, "name": "Sweden", "country": "Sweden", "short_description": "Sweden", "ultra_short_description": "Sweden" }
                ], 
 optionsAvailable:  {
-       "Reference": {"ccs": true, "bio": false, "opt2": false, "opt3": false},
-       "Share Optimism": {"ccs": true, "bio": false, "opt2": false, "opt3": false},
-       "Technology Optimism": { "ccs": true, "bio": false, "opt2": false, "opt3": false  }
+       "Reference": {"ccs": true, "opt1": false, "opt2": false, "opt3": false},
+       "Share Optimism": {"ccs": true, "opt1": false, "opt2": false, "opt3": false},
+       "Technology Optimism": { "ccs": true, "opt1": false, "opt2": false, "opt3": false  }
     }
   }
 };
